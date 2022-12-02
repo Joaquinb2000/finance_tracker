1421059382f0309787298d8f7f8e8200166d3366
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-12-01 22:33:28 -0400

    Adding first_name and last_name columns to users table, and enabling edit and creation for user devise model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_create_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_edit_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end

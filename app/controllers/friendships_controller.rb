4ced269882f9603b69e4e1449665c227f37aaec3
Joaquinb2000 <joaquinberon2000@gmail.com>
2022-12-02 02:20:31 -0400

    Adding destroy action for FriendshipsController

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,7 +4,9 @@ class FriendshipsController < ApplicationController
     @friend = User.find_by(email: email)
     respond_to do |format|
       if @friend
-        valid_action
+        friendship = Friendship.new( user_id: current_user.id, friend_id: @friend.id )
+        flash.now[:alert] = friendship.errors.full_messages.join("\n") if !friendship.valid?
+        @friend = nil if !flash.now[:alert].nil?
       else
         flash.now[:alert] = email == "" ? "Email can't be blank" : "User does not exist"
       end
@@ -29,11 +31,16 @@ class FriendshipsController < ApplicationController
     end
   end
 
-  private
-
-  def valid_action
-    friendship = Friendship.new( user_id: current_user.id, friend_id: @friend.id )
-    flash.now[:alert] = friendship.errors.full_messages.join("\n") if !friendship.valid?
-    @friend = nil if !flash.now[:alert].nil?
+  def destroy
+    friendship = Friendship.find_by(user_id: current_user.id, friend_id: params[:id])
+    friend = User.find(params[:id])
+    if friendship
+      friendship.destroy
+      flash[:success] = "Stopped following #{friend.email}"
+    else
+      flash[:alert] = "Users are not friends"
+    end
+    redirect_to my_friends_path
   end
+
 end
